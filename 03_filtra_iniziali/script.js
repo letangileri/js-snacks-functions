a473d951829676ff07e3780424c38ee4f11bd659
letangileri <let.angileri@gmail.com>
2025-07-18 18:01:23 +0200

    excercize 3

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -3,11 +3,40 @@
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
-// Dichiara la funzione qui.
 
+// Dichiara la funzione qui.
+function getStringFromLetter(string, charUser){
+    let charOfString = "";
+    let namesOfLetter = [];
+    for (let i = 0; i < string.length; i++){
+        const valueString = string[i];
+        charOfString = valueString.charAt(0);
+        if (charOfString === charUser){
+            namesOfLetter.push(valueString);
+        }
+    }
+    return namesOfLetter;
+}
+
+//arrow function
+const getStringFromLetter2 = (string, charUser) => {
+    let charOfString = "";
+    let namesOfLetter = [];
+    for (let i = 0; i < string.length; i++){
+        const valueString = string[i];
+        charOfString = valueString.charAt(0);
+        if (charOfString === charUser){
+            namesOfLetter.push(valueString);
+        }
+    }
+    return namesOfLetter;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
+const checkgetStringFromLetter = getStringFromLetter(names, "A");
+console.log(checkgetStringFromLetter);
 
-
+//arrow function
+console.log(getStringFromLetter2(names, "L"));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
