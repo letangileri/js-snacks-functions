c7fd4ef0f8b7eb3aab5db2699fd0efd3ab92c8fa
letangileri <let.angileri@gmail.com>
2025-07-18 17:25:41 +0200

    excercize one

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -4,10 +4,17 @@ const userName = 'Mario';
 
 
 // Dichiara la funzione qui.
+function getName (name){
+    return `Ciao ${name}`;
+}
 
+const getName2 = name => `Ciao ${name}`;
 
 // Invoca la funzione qui e stampa il risultato in console
+const callGetName = getName ("Mario");
+console.log(callGetName);
 
+console.log(getName2("Mario"));
 
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
