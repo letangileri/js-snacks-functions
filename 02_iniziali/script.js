b065c9136f9b6df5afc67b51e97c79e666ff13a8
letangileri <let.angileri@gmail.com>
2025-07-18 17:45:03 +0200

    excersize two

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,35 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
-
 // Dichiara la funzione qui.
-
+function getCharlistNames (names){
+    let charsOfNames = [];
+    let char = "";
+    for (let i = 0; i < names.length; i++){
+        const valueName = names[i];
+        char = valueName.charAt(0);
+        charsOfNames.push(char);
+    }
+    return charsOfNames;
+}
+//arrow function
+const getCharlistNames2 = names =>{
+    let charsOfNames = [];
+    let char = "";
+    for (let i = 0; i < names.length; i++){
+        const valueName = names[i];
+        char = valueName.charAt(0);
+        charsOfNames.push(char);
+    }
+    return charsOfNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const checkgetCharlistNames = getCharlistNames(names);
+console.log(checkgetCharlistNames);
 
+//arrow function
+console.log(getCharlistNames2(names));
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
